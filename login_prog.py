9a468ba395c99dbcf357d97bd346981315f56101
mball <mball7878@gmail.com>
2026-02-11 12:18:35 -0800

    Update flow and fixed bugs

--- login_prog.py
+++ login_prog.py
@@ -11,121 +11,127 @@ import json
 import ast
 from xml.etree.ElementInclude import FatalIncludeError
 
-filename = "login_dict.json"
+FILENAME = "login_dict.json"
 
-def load_convert():
+def retrieve_dict():
     """
-    Opens file login_dict.json converts strings in to list of dictionaries and returns the list
+        Opens file login_dict.json converts strings in to list of dictionaries
+     and returns the list.
     """
 
     try:
-        with open(filename, "r") as file:
+        with open(FILENAME, "r") as file:
             login_string = file.read()
-        login_list = json.loads(login_string)
+            if login_string:
+                login_list = json.loads(login_string)
+            else:
+                print( "empty file")
+                return "empty"
         return login_list
     except FileNotFoundError:
         print("file not found")
 
-''' if login_data:
-            for i in range(len(login_data)):
-                login_data_dict = ast.literal_eval(login_data[i])
-                login_dict_list.append(login_data_dict)
-            return login_dict_list
-        else:
-            return 'empty'
-'''
-
-def convert_write(dict_list):
+def save_dict(dict_list):
     """ Converts list of dictionaries to strings and writes to 'filename' """
 
     try:
-        with open(filename, "w") as file:
+        with open(FILENAME, "w") as file:
             json.dump(dict_list, file)
     except FileNotFoundError:
         print("file not found")
 
+def add_user(user_name, password):
 
-
-'''' tasks_dict_list = dict_list
-    for i in range(len(tasks_dict_list)):
-        data_list.append(str(tasks_dict_list[i]))
-    for i in range(len(data_list)):
-        data_list[i] = data_list[i] + '\n'
-    with open(filename, "w") as file:
-        for i in range(len(data_list)):
-            file.writelines(data_list[i])
-'''
+    login_obj = {"user": user_name, "P_word": password}
+    dict_list = retrieve_dict()
+    if dict_list != "empty":
+        dict_list.append(login_obj)
+        save_dict(dict_list)
+        print("another user added")
+    else:
+        dict_list = [login_obj]
+        save_dict(dict_list)
+        print("new user added")
 
 def sign_up():
+
     print(" Sign up for new account")
     user_name = input("Enter your user name: ")
+    print("Enter your new password. Password chars are hidden")
     password = getpass.getpass()
-    login_dict_list = load_convert()
-    print(f" password and username  {password}, {user_name}")
 
-    for i in range(len(login_dict_list)):
-        if login_dict_list[i].get("user") == user_name:
-            print(f" {user_name} user name already exists ")
-            sign_up()
-
-    if login_dict_list != 'empty':
-        login_obj = {"user": user_name, "P_word": password}
-        login_dict_list.append(login_obj)
-        convert_write(login_dict_list)
+    answer = check_user_name(user_name, password)
+    if answer == 'OK':
+        add_user(user_name, password)
+        return "OK"
+    elif answer == "Bad":
+        print("User name bad ")
+        action = input(" Press Enter to try again or q to quit ")
+        if action == 'q':
+            quit()
+        else:
+            login()
     else:
-        login_dict_list = []
-        login_obj = {"user": user_name, "P_word": password}
-        login_dict_list.append(login_obj)
-        convert_write(login_dict_list)
+        add_user(user_name, password)
+        return "OK"
 
-def check_login(user_name, password):
 
-    print(f" password and username  {password}, {user_name}")
-    login_dict_list = load_convert()
+def check_user_name (user_name, password):
+
+    dict_list = retrieve_dict()
+    if dict_list != 'empty':
+        for i in range(len(dict_list)):
+            if dict_list[i].get("user") == user_name:
+                print(f" {user_name} user name already exists ")
+                return "Bad"
+            else:
+                return "OK"
+    else:
+        print("No Login data ")
+
+
+
+def check_login():
+
+    user_name = input("Enter your user name: ")
+    password = input("Enter your password: ")
+    print(f" check login password and username  {password}, {user_name}")
+    login_dict_list = retrieve_dict()
 
     if login_dict_list != 'empty':
         for i in range(len(login_dict_list)):
             if login_dict_list[i].get("user") == user_name and login_dict_list[i].get("P_word") == password:
                 print(f" username {user_name}")
                 return "OK"
-        print("Login doesn't match \n")
-        end_program = input(" Press 'Return' to try again or press 'q' to quit ")
-        if end_program == 'q':
-            quit()
-        else:
-            sign_up()
-            return None
-
+            else:
+                return "Wrong password or username"
     else:
-        print("Login Data Not Found")
-        end_program = input(" press q to quict ")
-        if end_program == 'q':
-            quit()
-        else:
-            sign_up()
-            return None
+        return "Login Data Not Found"
 
 def login():
 
-    action = input(" Press 'Enter' to start, q to quit, "
+    action = input(" Press 'Enter' to login, q to quit,"
                         "or type 'new' to create a new account: ")
     if action == 'new':
         sign_up()
+        print("Sign up successful ")
         return "OK"
 
     elif action == 'q':
         quit()
 
     else:
-        user_name = input("Enter your username: ")
-        password = input("Enter your password: ")
-        user_status = check_login(user_name, password)
+        user_status = check_login()
         print(f" user status   {user_status}")
         if user_status == 'OK':
+            print("Login successful")
             return "OK"
         else:
-            print ("Login doesn't match ")
-        return None
+            action = input("Login doesn't match press 'Enter' to try again or press 'q' to quit ")
+            if action == 'q':
+                quit()
+            else:
+                check_login()
 
 if __name__ == "__main__":
     login()
